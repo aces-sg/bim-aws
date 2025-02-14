31783984c696ad2f370c922dc9b983dd9a5d9323
tangytang <tangjingyuanivan@gmail.com>
2025-02-14 16:08:26 +0800

    update stack name

--- bin/nginx.ts
+++ bin/nginx.ts
@@ -2,4 +2,4 @@ import { App } from "aws-cdk-lib";
 import { NginxProxyStack } from "../lib/nginx-proxy-stack"; // Ensure this path and name are correct
 
 const app = new App();
-new NginxProxyStack(app, "NginxProxyStack");
+new NginxProxyStack(app, "NginxProxyStackThree");
